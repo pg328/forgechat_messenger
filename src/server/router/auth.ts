a5d809f4552edb4ddf8c555e2c27a29beb9cb5ec
Philip George <23001002+pg328@users.noreply.github.com>
2022-07-31 01:07:50 +0100

    Removed  getSession query

--- src/server/router/auth.ts
+++ src/server/router/auth.ts
@@ -4,15 +4,15 @@ import {createRouter} from './context';
 export const authRouter = createRouter()
     .query('getSession', {
         resolve({ctx}) {
-            return ctx.session;
+            //return ctx.session;
         },
     })
     .middleware(async ({ctx, next}) => {
         // Any queries or mutations after this middleware will
         // raise an error unless there is a current session
-        if (!ctx.session) {
-            throw new TRPCError({code: 'UNAUTHORIZED'});
-        }
+        //if (!ctx.session) {
+        //    throw new TRPCError({code: 'UNAUTHORIZED'});
+        //}
         return next();
     })
     .query('getSecretMessage', {
